65cce3e96a7dcc990109e87d7b044f3e823b4d96
unknown <elmaharmsen01@gmail.com>
2019-08-22 14:50:50 +0200

    new stuff

--- jsfile.js
+++ jsfile.js
@@ -1,14 +1,19 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, "", {preload: preload, create: create, update: update, render: render});	
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, "", {preload: preload, create: create, update: update, render: render});	
 
 //var sky;	
 var player;
 var cursors;
+var score = 0,
+scoreText;
+var collectItems;
+var NUMBER_OF_ITEMS=10;
 
 function preload (){
     game.load.image('sky', 'images/sky4.png');
-
-    game.load.image('background','images/debug-grid-1920x1920.png');
-    game.load.image('player','images/phaser-dude.png');
+    game.load.image('background','images/bkg-02.png');
+    game.load.image('player','images/trump_B.png');
+    game.load.image('sun', 'images/sun-01.png');
+    game.load.image('green', 'images/plant_A.png');
 
 }
 
@@ -18,24 +23,49 @@ function create (){
 
     cursors = game.input.keyboard.createCursorKeys();*/
 
-    game.add.tileSprite(0, 0, 1920, 1920, 'background');
+    game.add.tileSprite(0, 0, 10000, 600, 'background');
 
-    game.world.setBounds(0, 0, 1920, 1920);
+    sun = game.add.image(60, 60, 'sun');
+    sun.scale.setTo(0.5,0.5);
+
+    game.world.setBounds(0, 0, 10000, 600);
 
     game.physics.startSystem(Phaser.Physics.P2JS);
 
-    player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+    player = game.add.sprite(200, 300, 'player'); 
+    player.scale.setTo(0.4,0.4);
 
-    game.physics.p2.enable(player);
+    game.physics.arcade.enable(player);
 
     cursors = game.input.keyboard.createCursorKeys();
 
     game.camera.follow(player);
+
+    /*scoreText = this.add.image(16, 16, 'green');
+    scoreText.scale.setTo(0.2, 0.2);*/
+
+    game.physics.arcade.enable(player);
+    player.body.gravity.y = 400;
+    player.body.collideWorldBounds = true;
+
+    itemSprites = [];
+    for (var i = 0; i < NUMBER_OF_ITEMS; i++) {
+        itemSprites[i] = game.add.sprite(0,0, 'green');
+        itemSprites[i].scale.setTo(0.2,0.2);
+        itemSprites[i].position = new Phaser.Point(game.rnd.frac() * 800, game.rnd.frac() * 600);
+    }
+    game.physics.enable(itemSprites, Phaser.Physics.ARCADE);
 }
+
+var screenCenter = new Phaser.Point(game.world.centerX, game.world.centerY);
+
+/*function collectItems (player, green){
+     
+}*/
 
 function update (){
 
-    player.body.setZeroVelocity();
+    /*player.body.setZeroVelocity();
 
     if (cursors.up.isDown)
     {
@@ -53,7 +83,7 @@ function update (){
     else if (cursors.right.isDown)
     {
         player.body.moveRight(300);
-    }
+    }*/
 
     //  Scroll the background
     //sky.tilePosition.y += 2;
